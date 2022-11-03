d8e6d551912dab55efa105db03ae0d1777bbe233
Gabriele <g.nocchi@hotmail.com>
2022-11-03 16:36:47 -0600

    added 500bp flanks which are removed at the end

--- 3_sweed_results_analysis.sh
+++ 3_sweed_results_analysis.sh
@@ -1,8 +1,8 @@
 ### need genes .gff file, chrom.txt file and all SweeD_Report files for each species
 
-# This is to take care of badly formatted gff with spaces/tabs in the ID field.
+# This is to take care of badly formatted gff with spaces/tabs in the ID field, and adding 500bp flanks each side of each gene
 
-awk '{OFS="\t"}{print $1,$2,$3,$4,$5,$6,$7,$8,"id=gene;"}' genes.gff > noid_genes.gff
+awk '{OFS="\t"}{print $1,$2,$3,$4-500,$5+500,$6,$7,$8,"id=gene;"}' genes.gff > noid_genes.gff
 
 # first I format SweeD outputs and add chrom/scaffold name to all files, while removing first 3 lines (empty line, garbage, header)
 while read p; do
@@ -27,8 +27,9 @@ Rscript emp.R
 
 
 # here first I add for each gene, the count of CLR scans - then I format the final file to only have gene name (given by chrom:start-end), empirical pvalue and number of CLR scans within the gene
+# also removing the 500bp flanks from each side of genes, as I need unflanked genes to match genes with orthogroups using the map we have
 
-/data/programs/bedtools2/bin/bedtools coverage -a genes_emp.gff -b all_emp.bed -counts | awk '{OFS="\t"}{print $1":"$4"-"$5,$9,$10}' > final_genes_analysis.txt
+/data/programs/bedtools2/bin/bedtools coverage -a genes_emp.gff -b all_emp.bed -counts | awk '{OFS="\t"}{print $1":"$4+500"-"$5-500,$9,$10}' > final_genes_analysis.txt
 
 
 # I use the script below to take the minimum emp pvalue for each gene and apply a dunn sidak corrrection to it, based on the nummber of CLR scan within each gene

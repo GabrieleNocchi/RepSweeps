f65f040cfa013f50aa493d97edcf5cc18f224593
Gabriele <g.nocchi@hotmail.com>
2022-11-28 11:35:19 -0700

    Add files via upload

---
+++ compare_5k_1k.sh
@@ -0,0 +1,19 @@
+while read p; do
+
+tail -n +4 SweeD_Report.savolainen_alyrata_scandinavia.vcf.gz_$p| awk -v var="$p" 'BEGIN {OFS="\t"} {print var, $1-5000, $1+5000, "CLRSCAN", $2}' | awk '{OFS="\t"}{if ($2 > 0)  print $1, $2, $3, $4, $5;else print $1, 0, $3, $4, $5;}' > formatted\_$p
+
+done < chrom.txt
+
+
+
+
+### Concatenate formatted outputs into 1 and remove all the formatted chrom/scaffolds
+cat formatted_* > all_5k.bed
+
+rm formatted*
+
+
+/data/programs/bedtools2/bin/bedtools intersect -a all_5k.bed -b all_1k.bed -wo | awk '{OFS="\t"}{print $1,$2,$3,$4,$5,$10}' | awk '{OFS="\t"}{print $1":"$2"-"$3,$2,$3,$4,$5,$6}'> to_plot_in_R.txt
+
+
+

57b1cf9e25cc228304d4f4515f7469a75255296a
Gabriele <g.nocchi@hotmail.com>
2022-11-28 11:44:06 -0700

    Add files via upload

--- compare_5kb_1kb/plot_5k_vs_1k.R
+++ compare_5kb_1kb/plot_5k_vs_1k.R
@@ -13,7 +13,8 @@ c <- cbind(b,a)
 
 colnames(c) <- c("window_ID1", "start", "end", "null","CLR_window", "minimum_CLR_window", "window_ID2", "mean_CLR")
 
+plot(-log10(b$V5), -log10(b$V6), xlab = "-log10 5kb CLR", ylab = "-log10 1kb_max_CLR")
+cor(b$V5,b$V6)
 
-plot(-log10(c$CLR_window), -log10(c$mean_CLR), xlab = "-log10 5kb", ylab = "-log10 1kb_average_CLR")
-plot(c$CLR_window, c$mean_CLR, xlim = c(0,10), ylim = c(0,10))
+plot(-log10(c$CLR_window), -log10(c$mean_CLR), xlab = "-log10 5kb CLR", ylab = "-log10 1kb_average_CLR")
 cor(c$CLR_window,c$mean_CLR)

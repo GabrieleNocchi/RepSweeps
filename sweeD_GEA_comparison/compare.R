b2e2103f545047d6a47eb4695ee00d4ac0e4af89
Gabriele <g.nocchi@hotmail.com>
2022-12-01 14:14:20 -0700

    added hypergeometric expectancy and new species

--- sweeD_GEA_comparison/compare.R
+++ sweeD_GEA_comparison/compare.R
@@ -1,8 +1,8 @@
 library(ComplexHeatmap)
 ##### change threshold accordingly
-thr <- 0.05
+thr <- 0.01
 ##### change wd accordingly
-setwd("/Users/gnocc/Desktop/projects/jim_results_and_ortho_map/")
+setwd("/Users/gnocc/Desktop/projects/jim_results_and_ortho_map/new_method_average_CLR/")
 
 ##### Preparing Gabriele top thr % orthogroup p values
 ##### wright_atuber
@@ -135,7 +135,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df <- data.frame(res_percent)
 colnames(df) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -272,8 +278,13 @@ jim <-jim[order(jim$Orthogroup),]
   test <- length(intersect(gab_outliers$orthogroup, x$X1))
   return(test)
  })
-
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df2 <- data.frame(res_percent)
 colnames(df2) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -412,7 +423,13 @@ rownames(df2) <- c("lowry_phallii")
    return(test)
   })
 
-  res_percent <- lapply(res,"/",length(annual_precip$X1))
+  # Hypergeometric expectancy
+  #res_percent <- lapply(res,"/",length(annual_precip$X1))
+  total <- length(unique(jim$Orthogroup))
+  percent <- 100/(thr * 100)
+  expect_outliers <- total/percent
+  hyper_exp <- (expect_outliers * expect_outliers)/total
+  res_percent <- lapply(res,"/",hyper_exp)
 
  df3 <- data.frame(res_percent)
  colnames(df3) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -550,7 +567,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df4 <- data.frame(res_percent)
 colnames(df4) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -691,7 +714,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df5 <- data.frame(res_percent)
 colnames(df5) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -829,7 +858,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df6 <- data.frame(res_percent)
 colnames(df6) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -968,7 +1003,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df7 <- data.frame(res_percent)
 colnames(df7) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -1106,7 +1147,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df8 <- data.frame(res_percent)
 colnames(df8) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -1244,7 +1291,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df9 <- data.frame(res_percent)
 colnames(df9) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -1382,7 +1435,13 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df10 <- data.frame(res_percent)
 colnames(df10) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
@@ -1391,7 +1450,7 @@ rownames(df10) <- c("murray_ealb")
 
 ##### Preparing Gabriele top thr % orthogroup p values
 ###### tiffin_truncatula
-gab <- read.table("tiffin_truncatula_final_orthogroup.txt", header=TRUE)
+gab <- read.table("tiffin_mtruncatula_final_orthogroup.txt", header=TRUE)
 
 gab_ortho_p <- gab[,7]
 
@@ -1520,15 +1579,167 @@ jim <-jim[order(jim$Orthogroup),]
   return(test)
  })
 
- res_percent <- lapply(res,"/",length(annual_precip$X1))
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
 
 df11 <- data.frame(res_percent)
 colnames(df11) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
 rownames(df11) <- c("tiffin_truncatula")
 
 
+
+
+##### Preparing Gabriele top thr % orthogroup p values
+###### ptremula
+gab <- read.table("ingvarsson_ptremula_final_orthogroup.txt", header=TRUE)
+
+gab_ortho_p <- gab[,7]
+
+
+assign.rank <- function(array){
+  pvalues <- array(0, length(array))
+
+  ordered.indexes <- order(array, decreasing = TRUE)
+
+  j <- length(array)
+  for( i in ordered.indexes ){
+    pvalues[i] <- j/length(array)
+    j <- j-1
+  }
+
+  return(pvalues)
+}
+
+
+gab_ranks <- assign.rank(gab_ortho_p)
+
+gab <- cbind(gab,gab_ranks)
+
+
+gab_outliers <- gab[gab$gab_ranks < thr,]
+
+
+##### Preparing Jim top thr % ortho p values
+jim <- readRDS("run220927_COMBINED25species_mirrored_221026_WZA_OG_pvals.rds")
+jim <- jim[jim$species == "Populus tremula",]
+
+jim <-jim[order(jim$Orthogroup),]
+
+ jim_ranks <- sapply(split(jim$min_sdP_DS, jim$climate), assign.rank)
+
+ jim_ranks <- data.frame(jim_ranks)
+
+ annual_precip <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$annual_precip))
+ annual_precip <- annual_precip[annual_precip$X2 < thr,]
+
+ isothermality <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$isothermality))
+ isothermality <- isothermality[isothermality$X2 < thr,]
+
+ max_temp_warmest_month <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$max_temp_warmest_month))
+ max_temp_warmest_month <- max_temp_warmest_month[max_temp_warmest_month$X2 < thr,]
+
+
+ mean_diurnal <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_diurnal))
+ mean_diurnal <- mean_diurnal[mean_diurnal$X2 < thr,]
+
+
+ mean_temp <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_temp))
+ mean_temp <- mean_temp[mean_temp$X2 < thr,]
+
+
+ mean_temp_cold_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_temp_cold_quarter))
+ mean_temp_cold_quarter <- mean_temp_cold_quarter[mean_temp_cold_quarter$X2 < thr,]
+
+
+ mean_temp_dry_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_temp_dry_quarter))
+ mean_temp_dry_quarter <- mean_temp_dry_quarter[mean_temp_dry_quarter$X2 < thr,]
+
+
+ mean_temp_warm_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_temp_warm_quarter))
+ mean_temp_warm_quarter <- mean_temp_warm_quarter[mean_temp_warm_quarter$X2 < thr,]
+
+
+ mean_temp_wet_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$mean_temp_wet_quarter))
+ mean_temp_wet_quarter <- mean_temp_wet_quarter[mean_temp_wet_quarter$X2 < thr,]
+
+
+ min_temp_coldest_month <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$min_temp_coldest_month))
+ min_temp_coldest_month <- min_temp_coldest_month[min_temp_coldest_month$X2 < thr,]
+
+
+ prec_clim_change <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$prec_clim_change))
+ prec_clim_change <- prec_clim_change[prec_clim_change$X2 < thr,]
+
+
+ precip_cold_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_cold_quarter))
+ precip_cold_quarter <- precip_cold_quarter[precip_cold_quarter$X2 < thr,]
+
+
+ precip_dry_month <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_dry_month))
+ precip_dry_month <- precip_dry_month[precip_dry_month$X2 < thr,]
+
+
+ precip_dry_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_dry_quarter))
+ precip_dry_quarter <- precip_dry_quarter[precip_dry_quarter$X2 < thr,]
+
+
+ precip_seasonality <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_seasonality))
+ precip_seasonality <- precip_seasonality[precip_seasonality$X2 < thr,]
+
+
+ precip_warm_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_warm_quarter))
+ precip_warm_quarter <- precip_warm_quarter[precip_warm_quarter$X2 < thr,]
+
+
+ precip_wet_month <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_wet_month))
+ precip_wet_month <- precip_wet_month[precip_wet_month$X2 < thr,]
+
+
+ precip_wet_quarter <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$precip_wet_quarter))
+ precip_wet_quarter <- precip_wet_quarter[precip_wet_quarter$X2 < thr,]
+
+
+ temp_range <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$temp_range))
+ temp_range <- temp_range[temp_range$X2 < thr,]
+
+
+ temp_seasonality <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$temp_seasonality))
+ temp_seasonality <- temp_seasonality[temp_seasonality$X2 < thr,]
+
+
+ tmax_clim_change <- data.frame(cbind(unique(jim$Orthogroup),jim_ranks$tmax_clim_change))
+ tmax_clim_change <- tmax_clim_change[tmax_clim_change$X2 < thr,]
+
+
+
+ jim_list <- list(annual_precip,isothermality, max_temp_warmest_month, mean_diurnal, mean_temp, mean_temp_cold_quarter, mean_temp_dry_quarter, mean_temp_warm_quarter, mean_temp_wet_quarter, min_temp_coldest_month, prec_clim_change,precip_cold_quarter,precip_dry_month,precip_dry_quarter,precip_seasonality,precip_warm_quarter,precip_wet_month,precip_wet_quarter,temp_range,temp_seasonality,tmax_clim_change)
+
+
+ res <- lapply(jim_list, function(x){
+  test <- length(intersect(gab_outliers$orthogroup, x$X1))
+  return(test)
+ })
+
+ # Hypergeometric expectancy
+ #res_percent <- lapply(res,"/",length(annual_precip$X1))
+ total <- length(unique(jim$Orthogroup))
+ percent <- 100/(thr * 100)
+ expect_outliers <- total/percent
+ hyper_exp <- (expect_outliers * expect_outliers)/total
+ res_percent <- lapply(res,"/",hyper_exp)
+
+df12 <- data.frame(res_percent)
+colnames(df12) <- c("annual_precip","isothermality", "max_temp_warmest_month", "mean_diurnal", "mean_temp", "mean_temp_cold_quarter", "mean_temp_dry_quarter", "mean_temp_warm_quarter", "mean_temp_wet_quarter", "min_temp_coldest_month", "prec_clim_change","precip_cold_quarter","precip_dry_month","precip_dry_quarter","precip_seasonality","precip_warm_quarter","precip_wet_month","precip_wet_quarter","temp_range","temp_seasonality","tmax_clim_change")
+rownames(df12) <- c("ingvarsson_ptremula")
+
+
 ##### Merging all output for all datasets together and heatmap generation
-final <- rbind(df,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11)
+final <- rbind(df,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12)
 final <- as.matrix(final)
 
 Heatmap(final, show_column_dend = FALSE,show_row_dend = FALSE,heatmap_legend_param = list(title = "", color_bar = "continuous"),row_names_side = "left")

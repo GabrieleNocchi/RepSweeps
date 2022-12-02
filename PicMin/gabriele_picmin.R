7013dadee274beebb66ed3a148aa7e8fb444980d
Gabriele <g.nocchi@hotmail.com>
2022-12-02 12:32:19 -0700

    Add files via upload

---
+++ PicMin/gabriele_picmin.R
@@ -0,0 +1,98 @@
+
+setwd("/Users/gnocc/Desktop/")
+lib <- c("cowplot","poolr","mvmeta","qvalue","tidyr","ape","VGAM","ggExtra","pbmcapply","parallel","wCorr","data.table","ggplot2","viridis","ggridges","dplyr","readr")
+sapply(lib,library,character.only=T)
+
+# Function Library --------------------------------------------------------
+source("picmin.R")
+
+
+############################################################################
+# Assemble the Orthofinder Outputs to Analyse
+n_cores = 1 # Number of cpu
+orthogroup_cutoff <- 5 # Minimum number of species in an OG
+# max_paralog_per_OG <- 10
+
+# Fetch the processed Orthogroup Pvals
+OG_pvals <- readRDS("orthogroup_results.rds")
+OG_pvals <- as.data.table(OG_pvals)
+
+##################################################
+
+# Define climate variaibles here
+climate_vars <- unique(OG_pvals$climate)
+focal_climate <- "mean_temp"
+OG_maxP <- OG_pvals[climate == focal_climate,]
+focal_datasets_climate <- unique(OG_pvals$species)
+
+
+
+# Remove orthogroups with not enough datasets...
+remove_small_OG = names(table(OG_maxP$Orthogroup)[table(OG_maxP$Orthogroup) < orthogroup_cutoff])
+
+# Remove orthogroups with too many paralogs in any species...
+# remove_paralogs = unique(OG_maxP[Ngenes_per_species > max_paralog_per_OG,Orthogroup])
+
+# Filter
+OG_maxP_test <- OG_maxP[!OG_maxP$Orthogroup %in% unique(remove_small_OG),]
+
+# And get minP
+minPval_OG <- OG_maxP_test[,.(minP=min(max_sdP_DS)),by=Orthogroup]
+
+picmin_cor_mats <- pbmclapply(3:length(focal_datasets_climate),function(n){
+
+  ee <- array (NA,c(10000,n))
+  for (i in 1:n){
+    temp <- runif(10000)
+    ee[,i] <- EmpiricalPs(temp)
+  }
+  emp_p_null_dat30_unif <- ee
+
+  for (kk in 1:nrow(emp_p_null_dat30_unif)){
+    ind1 <- order(emp_p_null_dat30_unif[kk,])
+    emp_p_null_dat30_unif [kk,] <- emp_p_null_dat30_unif[kk,ind1]
+  }
+
+  cor_ord_unif <- cor (emp_p_null_dat30_unif[,2:n])
+  cor_ord_unif
+},mc.cores=n_cores)
+
+
+names(picmin_cor_mats) <- as.character(3:length(focal_datasets_climate))
+
+
+picmin_res <- rbindlist(pbmclapply(unique(OG_maxP_test$Orthogroup),function(OG){
+  set.seed(1000)
+  tmp <- OG_maxP_test[OG_maxP_test$Orthogroup == OG,]
+  # Run first with dunn-sidak correction
+  # first_run = PicMin_bugfix(tmp$max_sdP_DS, picmin_cor_mats[[as.character(nrow(tmp))]], numReps = 100,correction = "dunn-sidak")
+  first_run = PicMin_bugfix(tmp$max_sdP_DS, picmin_cor_mats[[as.character(nrow(tmp))]], numReps = 1000,correction = "tippett")
+
+  if(first_run$p > 0.1){
+    return(first_run)
+  } else {
+    # # How many iterations to run Tippett
+    second_run_iter = ifelse(10/first_run$p > 1e8, 1e8, round(10/first_run$p)*10)
+    # # Run
+    second_run = PicMin_bugfix(tmp$max_sdP_DS, picmin_cor_mats[[as.character(nrow(tmp))]], numReps = second_run_iter,correction = "tippett")
+    return(second_run)
+  }
+},mc.cores=n_cores))
+
+
+picmin_res$picmin_fdr <- p.adjust(picmin_res$p,method = "fdr")
+picmin_res$Orthogroup <- unique(OG_maxP_test$Orthogroup)
+picmin_res$climate_var <- focal_climate
+
+
+
+  picmin_res[picmin_res$p == min(picmin_res$p),]
+  picmin_res[order(p),]
+
+  # Merge with the minP as well
+  picmin_res = merge(picmin_res,minPval_OG,by = "Orthogroup")
+
+  # Save these
+  picmin_res_output <- list(OG_maxP=OG_maxP_test,
+                            picmin_res=picmin_res)
+  saveRDS(picmin_res_output, "gab_picmin_results.rds")
---
+++ PicMin/join_results.R
@@ -0,0 +1,57 @@
+a <- read.table("kubota_ahalleri_final_orthogroup.txt", h = T)
+species <- rep("ahalleri", length(a$orthogroup))
+a <- cbind(a,species)
+
+b <- read.table("ingvarsson_ptremula_final_orthogroup.txt", h = T)
+species <- rep("ptremula", length(b$orthogroup))
+b <- cbind(b,species)
+
+c <- read.table("lowry_phalli_final_orthogroup.txt", h = T)
+species <- rep("phalli", length(c$orthogroup))
+c <- cbind(c,species)
+
+
+d <- read.table("mitchell_bstricta_final_orthogroup.txt", h = T)
+species <- rep("bstricta", length(d$orthogroup))
+d <- cbind(d,species)
+
+e <- read.table("murray_ealb_final_orthogroup.txt", h = T)
+species <- rep("ealb", length(e$orthogroup))
+e <- cbind(e,species)
+
+f <- read.table("murray_esid_final_orthogroup.txt", h = T)
+species <- rep("esid", length(f$orthogroup))
+f <- cbind(f,species)
+
+g <- read.table("murray_emag_final_orthogroup.txt", h = T)
+species <- rep("emag", length(g$orthogroup))
+g <- cbind(g,species)
+
+h <- read.table("savolainen_alyrata_final_orthogroup.txt", h = T)
+species <- rep("alyrata", length(h$orthogroup))
+h <- cbind(h,species)
+
+
+i <- read.table("tiffin_mtruncatula_final_orthogroup.txt", h = T)
+species <- rep("mtruncatula", length(i$orthogroup))
+i <- cbind(i,species)
+
+j <- read.table("weigel_athaliana_IBE_final_orthogroup.txt", h = T)
+species <- rep("athaliana", length(j$orthogroup))
+j <- cbind(j,species)
+
+k <- read.table("weiger_capsella_final_orthogroup.txt", h = T)
+species <- rep("capsella", length(k$orthogroup))
+k <- cbind(k,species)
+
+l <- read.table("wright_atuber_final_orthogroup.txt", h = T)
+species <- rep("atuber", length(l$orthogroup))
+l <- cbind(l,species)
+
+
+results <- rbind(a,b,c,d,e,f,g,h,j,k,l)
+climate <- rep("mean_temp", length(results$species))
+
+results <- cbind(results,climate)
+colnames(results) <- c("gene","min_CLR","scan_n","mean_emp_p","Orthogroup","ortho_size", "max_sdP_DS", "species", "climate")
+saveRDS(results, "orthogroup_results.rds")

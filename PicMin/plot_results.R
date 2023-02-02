b69d0e3fd1884d3976c1bfa1ffe956506f5b65e5
Gabriele <g.nocchi@hotmail.com>
2023-02-02 13:01:57 -0700

    Add files via upload

--- PicMin/plot_results.R
+++ PicMin/plot_results.R
@@ -26,8 +26,8 @@ picmin <- rep("PicMin", 5)
 
 my_data <- data.frame(count, FDR, picmin)
 
-ggplot(my_data, aes(fill=FDR, y=count, x=picmin)) +
-    geom_bar(position="stack", stat="identity", width = 0.25) + theme_classic() +
+p1 <- ggplot(my_data, aes(fill=FDR, y=count, x=picmin)) +
+    geom_bar(position="stack", stat="identity", width = 0.5) + theme_classic() +
   theme(axis.title.y=element_blank(),
         axis.line.y=element_blank(),
         axis.text.y=element_blank(),
@@ -36,3 +36,66 @@ ggplot(my_data, aes(fill=FDR, y=count, x=picmin)) +
         coord_flip() +
         scale_y_continuous(breaks=seq(0,280,20), position = "right") +
         ggtitle(paste("PicMin OGs = ", format(round(as.numeric(length(data$p)), 1), big.mark=",")))
+
+
+
+
+
+
+library(ggplot2)
+library(RColorBrewer)
+
+pic <- readRDS("gab_picmin_results.rds")
+pic <- pic$picmin_res
+pic <- pic[pic$picmin_fdr < 0.5,]
+results <- readRDS("orthogroup_results.rds")
+
+
+library(dplyr)
+data <- pic %>% left_join(results, by = c("Orthogroup"))
+mycolors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Paired", n = 6))
+
+p2<- ggplot(data, aes(x=reorder(species, ortho_DS, FUN = median), y=ortho_DS, fill = species)) +
+    geom_boxplot(width = 0.5) + theme_classic()  + theme(legend.position = "none") + ylab("Ortho_DS\n") + ggtitle("Distribution of the OGs DS corrected emp p-values across species for the PicMin OGs with FDR < 0.5 (280 OGs)") + xlab("Species") +
+    scale_fill_manual(values = mycolors)
+
+library(dplyr)
+low_p_count <- data %>% group_by(species) %>% count(ortho_DS < 0.1)
+low_p_count <- data.frame(low_p_count)
+colnames(low_p_count) <- c("species", "count", "n")
+
+
+below <- low_p_count[low_p_count$count == "TRUE",]
+above <- low_p_count[low_p_count$count == "FALSE",]
+
+below <- cbind (below$species, below$n)
+colnames(below) <- c("species", "below")
+
+above <- cbind(above$species, above$n)
+colnames(above) <- c("species", "above")
+
+
+above <- as.data.frame(above)
+below <- as.data.frame(below)
+
+reformatted_data <- below %>% left_join(above, by = c("species"))
+
+
+contribution <- as.numeric(reformatted_data$below) / (as.numeric(reformatted_data$below)+ as.numeric(reformatted_data$above))
+
+final_data <- cbind(reformatted_data,contribution)
+
+
+library(ggplot2)
+
+p3 <- ggplot(final_data, aes(x = reorder(species, contribution),y = 1, fill = contribution)) + scale_fill_gradient(low="grey", high="blue") + theme_classic() +
+  geom_tile(height =0.5) +
+  theme(axis.title.y=element_blank(),
+        axis.line.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+        xlab("species") + labs(fill='Species contribution') + ggtitle("Species contribution to PicMin OGs with FDR < 0.5 = N(OG p< 0.1)/N(OG p)")
+
+
+library(gridExtra)
+grid.arrange(p1,p2,p3,ncol=1)

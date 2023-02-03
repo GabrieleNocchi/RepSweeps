d3baf7fe76b2c90a54df8cb4fdeb291d4e77d52c
Gabriele <g.nocchi@hotmail.com>
2023-02-03 10:43:32 -0700

    Add files via upload

--- PicMin/ortho_paralogues.R
+++ PicMin/ortho_paralogues.R
@@ -10,6 +10,51 @@ my_data <- results %>% group_by(species, ortho_size) %>%
 
 
 
-    ggplot(my_data, aes(fill=as.factor(ortho_size), y=total_count, x=species)) +
+  p1 <-   ggplot(my_data, aes(fill=as.factor(ortho_size), y=total_count, x=species)) +
     geom_bar(stat="identity", width = 0.8, position = position_stack(reverse = TRUE)) + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = -1) +
-    labs(fill='Paralogues N') + ylab("Number") + xlab("Species") + ggtitle("Paralogues Number per Orthogroup")
+    labs(fill='Paralogues N') + ylab("Number") + xlab("Species") + ggtitle("Paralogues Number per Orthogroup") +
+theme(legend.key.size = unit(0.2, 'cm'), plot.title = element_text(size = 10, face = "bold"))
+
+
+
+
+    my_data_2 <- results %>% group_by(Orthogroup) %>%
+    summarise(total_count=n())
+
+
+    my_data_2 <- data.frame(my_data_2)
+
+    total <- table(my_data_2$total_count)
+
+    total <- as.data.frame(total)
+
+
+    p2 <- ggplot(data=total, aes(x=Var1, y=Freq, fill = Var1)) +
+    geom_bar(stat="identity") + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = -1) + xlab("Number of Orthologues") + ylab("Number") + ggtitle("Total Number of OGs by orthologues Number\nPicMin used only OGs with at least 7 Orthologues")+
+      labs(fill='Orthologues N') +
+  theme(legend.key.size = unit(0.2, 'cm'),plot.title = element_text(size = 10, face = "bold"))
+
+    ortho_species <- results %>% left_join(my_data_2, by = c("Orthogroup"))
+
+    my_data_3 <- ortho_species %>% group_by(species, total_count) %>%
+        summarise(number=n())
+
+   my_data_3 <- as.data.frame(my_data_3)
+
+
+
+
+
+    p3 <- ggplot(my_data_3, aes(fill=as.factor(total_count), y=number, x=species)) +
+    geom_bar(stat="identity", width = 0.8) + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = -1) +
+    labs(fill='Species N') + ylab("Number") + xlab("Species") + ggtitle("Species Number per Orthogroup in each Species") +
+theme(legend.key.size = unit(0.2, 'cm'),plot.title = element_text(size = 10, face = "bold"))
+
+
+
+
+
+pdf("OGs_assement.pdf")
+library(gridExtra)
+grid.arrange(p1,p3,p2,ncol=1)
+dev.off()

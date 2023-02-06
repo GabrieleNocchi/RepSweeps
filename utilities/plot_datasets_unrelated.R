616f57fd25409c6672922b17dc9c0b16c1983584
Gabriele <g.nocchi@hotmail.com>
2023-02-06 11:07:21 -0700

    Add files via upload

--- utilities/plot_datasets_unrelated.R
+++ utilities/plot_datasets_unrelated.R
@@ -4,10 +4,10 @@ library(ggplot2)
 a <- read.table("unrelated_to_plot.txt", sep = "\t", h = T)
 
 
-mycolors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Paired", n = 6))
+mycolors = c("lightgoldenrod1", "darkgrey")
 
 
 
 ggplot(a, aes(fill=cat, y=count, x=genome)) +
   geom_bar(position="stack", stat="identity") + coord_flip() + theme_classic() + xlab("Genome") + ylab("Individuals Count") +
-    scale_fill_manual(values = mycolors) + labs(fill='Individuals') + ggtitle("Number of selected individuals per dataset kin < 0.2") 
+    scale_fill_viridis_d(option="cividis") + labs(fill='Individuals') + ggtitle("Number of selected individuals per dataset kin < 0.2")

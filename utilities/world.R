155bbeedfc832c7a97b023fb7625147abd16ea0d
Gabriele <g.nocchi@hotmail.com>
2023-02-17 13:49:34 -0700

    Add files via upload

--- utilities/world.R
+++ utilities/world.R
@@ -2,7 +2,8 @@ setwd("/Users/gnocc/Desktop/RepSweeps/world_map/data/")
 library("rnaturalearth")
 library("rnaturalearthdata")
 library(RColorBrewer)
-
+library(data.table)
+library(ggplot2)
 world <- ne_countries(scale = "medium", returnclass = "sf")
 class(world)
 
@@ -11,13 +12,13 @@ class(world)
 
 
     list_csv_files <- list.files(path = "/Users/gnocc/Desktop/RepSweeps/world_map/data/")
-    df2 = do.call(rbind, lapply(list_csv_files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+    df2 = do.call(rbind, lapply(list_csv_files, function(x) fread(x, stringsAsFactors = FALSE, h = F, sep = ",",select=c(1,2,3,4,5,6,7))))
     df2
 
 mycolors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Paired", n = 6))
 
 ggplot(data = world) +
-    geom_sf() + theme_classic() + geom_point(data = df2, aes(x = lon, y = lat, col = species), size = 0.5,
+    geom_sf() + theme_classic() + geom_point(data = df2, aes(x = V7, y = V6, col = V1), size = 0.5,
         shape = 19) + scale_color_manual(values = mycolors)+ guides(colour = guide_legend(override.aes = list(size=5))) +
         theme(axis.title.y=element_blank(),
         axis.line.y=element_blank(),

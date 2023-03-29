7342dc14f405fcbbe9aa712524d2e9e6a8bc8663
Gabriele <g.nocchi@hotmail.com>
2023-03-29 10:48:30 -0600

    Add files via upload

--- utilities/world.R
+++ utilities/world.R
@@ -15,10 +15,12 @@ class(world)
     df2 = do.call(rbind, lapply(list_csv_files, function(x) fread(x, stringsAsFactors = FALSE, h = F, sep = ",",select=c(1,2,3,4,5,6,7))))
     df2
 
-mycolors = c("#1B9E77","#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D" ,"#666666", "#A6CEE3", "#1F78B4", "#B2DF8A", "cadetblue", "#FB9A99", "#E31A1C")
 
+mycolors <- c("#E69F00", "#56B4E9", "#009E73", "red", "#0072B2", "#D55E00",
+                          "#CC79A7", "#999999", "#000000", "brown", "#8EBA42", "#BEBADA",
+                          "#FB8072", "#80B1D3", "#FDB462", "navy", "#FCCDE5")
 ggplot(data = world) +
-    geom_sf() + theme_classic() + geom_point(data = df2, aes(x = V7, y = V6, col = V1), size = 0.5,
+    geom_sf(fill = "antiquewhite") + theme_classic() + geom_point(data = df2, aes(x = V7, y = V6, col = V1), size = 0.5,
         shape = 19) + scale_color_manual(values = mycolors)+ guides(colour = guide_legend(override.aes = list(size=5))) +
         theme(axis.title.y=element_blank(),
         axis.line.y=element_blank(),
@@ -27,4 +29,5 @@ ggplot(data = world) +
         axis.title.x=element_blank(),
         axis.line.x=element_blank(),
         axis.text.x=element_blank(),
+        panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.5), panel.background = element_rect(fill = "aliceblue"),
         axis.ticks.x=element_blank(), plot.title = element_text(hjust = 0.5)) + labs(col='Species') + ggtitle("RepSweeps WGS Datasets")

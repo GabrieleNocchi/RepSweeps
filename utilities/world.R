b8a04f262269b6a002aee29009bcfc50ceb6528e
Gabriele <g.nocchi@hotmail.com>
2023-03-30 15:56:31 -0600

    Add files via upload

--- utilities/world.R
+++ utilities/world.R
@@ -1,4 +1,4 @@
-setwd("/Users/gnocc/Desktop/RepSweeps/world_map/data/")
+setwd("/Users/gnocc/Desktop/RepSweeps/RepSweeps_17_Gab/world_map/data")
 library("rnaturalearth")
 library("rnaturalearthdata")
 library(RColorBrewer)
@@ -11,16 +11,14 @@ class(world)
 
 
 
-    list_csv_files <- list.files(path = "/Users/gnocc/Desktop/RepSweeps/world_map/data/")
+    list_csv_files <- list.files(path = "/Users/gnocc/Desktop/RepSweeps/RepSweeps_17_Gab/world_map/data")
     df2 = do.call(rbind, lapply(list_csv_files, function(x) fread(x, stringsAsFactors = FALSE, h = F, sep = ",",select=c(1,2,3,4,5,6,7))))
     df2
 
 
-mycolors <- c("#E69F00", "#56B4E9", "#009E73", "red", "#0072B2", "#D55E00",
-                          "#CC79A7", "#999999", "#000000", "brown", "#8EBA42", "#BEBADA",
-                          "#FB8072", "#80B1D3", "#FDB462", "navy", "#FCCDE5")
+mycolors <- c("#1F77B4", "#FF7F0E", "#2CA02C", "#D62728", "#9467BD", "#8C564B", "#E377C2", "#7F7F7F", "#BCBD22", "#17BECF", "#393B79", "#637939", "#8C6D31", "gold", "#4A7EBB", "#AEC7E8", "#FFBB78")
 ggplot(data = world) +
-    geom_sf(fill = "antiquewhite") + theme_classic() + geom_point(data = df2, aes(x = V7, y = V6, col = V1), size = 0.5,
+        geom_sf(fill = "antiquewhite") + theme_classic() + geom_point(data = df2, aes(x = V7, y = V6, col = V1), size = 0.5,
         shape = 19) + scale_color_manual(values = mycolors)+ guides(colour = guide_legend(override.aes = list(size=5))) +
         theme(axis.title.y=element_blank(),
         axis.line.y=element_blank(),

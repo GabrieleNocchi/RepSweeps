0a0aef298971d47c73e0875f2ea0f2e6e851e2f3
Gabriele <g.nocchi@hotmail.com>
2023-03-30 16:03:10 -0600

    Add files via upload

--- utilities/plot_datasets_unrelated.R
+++ utilities/plot_datasets_unrelated.R
@@ -1,7 +1,7 @@
 library(RColorBrewer)
 library(ggplot2)
 
-a <- read.table("unrelated_to_plot.txt", sep = "\t", h = T)
+a <- read.table("unrelated_to_plot.txt", sep = ",", h = T)
 
 
 mycolors = c("lightgoldenrod1", "darkgrey")
@@ -10,4 +10,4 @@ mycolors = c("lightgoldenrod1", "darkgrey")
 
 ggplot(a, aes(fill=cat, y=count, x=genome)) +
   geom_bar(position="stack", stat="identity") + coord_flip() + theme_classic() + xlab("Genome") + ylab("Individuals Count") +
-    scale_fill_viridis_d(option="cividis") + labs(fill='Individuals') + ggtitle("Number of selected individuals per dataset kin < 0.2")
+    scale_fill_viridis_d(option="mako") + labs(fill='Individuals') + ggtitle("Number of selected individuals per dataset kin < 0.2")

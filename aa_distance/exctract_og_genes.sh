e18fe80a7d0db486156a96a6207c39af4bc8df3c
Gabriele <g.nocchi@hotmail.com>
2023-04-11 17:07:49 -0600

    Add files via upload

---
+++ aa_distance/exctract_og_genes.sh
@@ -0,0 +1,14 @@
+for file in ./OG*
+do
+	sed 's/:/___/g' $file > tmp.txt
+
+	while read line
+		do
+    		grep -A 1 -h $line *faa >> $file\_extracted_genes.txt
+    		# add commands to process the line here
+	done < tmp.txt
+	
+done
+
+
+rm tmp.txt

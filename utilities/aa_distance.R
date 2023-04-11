fd038b140558dc779830e5128e6205c736c8ae45
Gabriele <g.nocchi@hotmail.com>
2023-04-11 16:48:25 -0600

    Add files via upload

---
+++ utilities/aa_distance.R
@@ -0,0 +1,12 @@
+a <- readRDS("gab_picmin_results.rds")
+a <- a$picmin_res
+a <- a[a$picmin_fdr < 0.5,]
+a <- a[,1]
+
+my_results <- readRDS("orthogroup_results.rds")
+
+for (i in 1:length(a)) {
+    selection <- my_results[my_results$Orthogroup == a[i],]
+    genes <- selection$gene
+    write.table(genes,file = a[i], quote = FALSE, row.names = F, col.names = F)
+}

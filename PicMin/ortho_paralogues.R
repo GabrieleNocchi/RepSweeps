70b315fcc5806019273ebb82ee6eb9e58d6a7727
Gabriele <g.nocchi@hotmail.com>
2023-04-13 13:15:09 -0600

    Add files via upload

--- PicMin/ortho_paralogues.R
+++ PicMin/ortho_paralogues.R
@@ -2,6 +2,23 @@ library(dplyr)
 library(ggplot2)
 
 results <- readRDS("orthogroup_results.rds")
+results$species <- sub("ptricho", "P. trichocarpa", results$species)
+results$species <- sub("emag", "E. magnificata", results$species)
+results$species <- sub("ealb", "E. albens", results$species)
+results$species <- sub("esid", "E. sideroxylon", results$species)
+results$species <- sub("bplaty", "B. platyphylla", results$species)
+results$species <- sub("bpendula", "B. pendula", results$species)
+results$species <- sub("hann", "H. annus", results$species)
+results$species <- sub("hargo", "H. argophyllus", results$species)
+results$species <- sub("hpet", "H. petiolaris", results$species)
+results$species <- sub("capsella", "C. rubella", results$species)
+results$species <- sub("ahalleri", "A. halleri", results$species)
+results$species <- sub("athaliana", "A. thaliana", results$species)
+results$species <- sub("ptremula", "P. tremula", results$species)
+results$species <- sub("bstricta", "B. stricta", results$species)
+results$species <- sub("phalli", "P. hallii", results$species)
+results$species <- sub("atuber", "A. tuberculatus", results$species)
+results$species <- sub("mtruncatula", "M. truncatula", results$species)
 
 my_data <- results %>% group_by(species, ortho_size) %>% summarise(total_count=n())
 
@@ -11,7 +28,7 @@ my_data <- as.data.frame(my_data)
 
 p1 <-   ggplot(my_data, aes(fill=as.factor(ortho_size), y=total_count, x=species)) +
         geom_bar(stat="identity", width = 0.8, position = position_stack(reverse = TRUE)) + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = -1, option  = "mako") +
-        labs(fill='Paralogues N') + ylab("Number") + xlab("Species") + ggtitle("Paralogues Number per Orthogroup") +
+        labs(fill='Paralogues N') + ylab("Number") + xlab("Species") + ggtitle("Paralogues number per orthogroup") +
         theme(legend.key.size = unit(0.2, 'cm'), plot.title = element_text(size = 10, face = "bold"))
 
 
@@ -26,10 +43,9 @@ total <- table(my_data_2$total_count)
 
 total <- as.data.frame(total)
 
-p2 <- ggplot(data=total, aes(x=Var1, y=Freq, fill = Var1)) +
-      geom_bar(stat="identity") + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = -1, option  = "mako") + xlab("Number of Species") + ylab("Number") + ggtitle("OGs tested in PicMin by species number\nPicMin tested only OGs found in at least 7 species")+
-      labs(fill='Species N') +
-      theme(legend.key.size = unit(0.2, 'cm'),plot.title = element_text(size = 10, face = "bold"))
+p2 <- ggplot(data=total, aes(x=Var1, y=Freq, fill = "constant")) +
+      geom_bar(stat="identity", color= "black", width = 0.8,linewidth = 0.3) + theme_classic() + scale_fill_viridis_d(direction = -1, option  = "mako") + xlab("Number of Species") + ylab("Number") + ggtitle("OGs tested in PicMin by species number")+
+      theme(plot.title = element_text(size = 10, face = "bold"), legend.position = "none")
 
 ortho_species <- results %>% left_join(my_data_2, by = c("Orthogroup"))
 
@@ -40,7 +56,7 @@ my_data_3 <- as.data.frame(my_data_3)
 
 p3 <- ggplot(my_data_3, aes(fill=as.factor(total_count), y=number, x=species)) +
       geom_bar(stat="identity", width = 0.8) + coord_flip() + theme_classic() + scale_fill_viridis_d(direction = 1, option  = "mako") +
-      labs(fill='Species N') + ylab("Number") + xlab("Species") + ggtitle("Species Number per Orthogroup in each Species") +
+      labs(fill='Species N') + ylab("Number") + xlab("Species") + ggtitle("Species number per orthogroup in each species") +
       theme(legend.key.size = unit(0.2, 'cm'),plot.title = element_text(size = 10, face = "bold"))
 
 
--- PicMin/plot_results.R
+++ PicMin/plot_results.R
@@ -24,7 +24,7 @@ picmin <- rep("PicMin", 5)
 my_data <- data.frame(count, FDR, picmin)
 
 p1 <- ggplot(my_data, aes(fill=FDR, y=count, x=picmin)) +
-      geom_bar(position="stack", stat="identity", width = 0.5) + theme_classic() +
+      geom_bar(position="stack", stat="identity", width = 0.5,linewidth = 0.3, colour = "black") + theme_classic() +
       theme(axis.title.y=element_blank(),
         axis.line.y=element_blank(),
         axis.text.y=element_blank(),
@@ -50,13 +50,32 @@ pic <- pic[pic$picmin_fdr < 0.5,]
 results <- readRDS("orthogroup_results.rds")
 
 
+
+results$species <- sub("ptricho", "P. trichocarpa", results$species)
+results$species <- sub("emag", "E. magnificata", results$species)
+results$species <- sub("ealb", "E. albens", results$species)
+results$species <- sub("esid", "E. sideroxylon", results$species)
+results$species <- sub("bplaty", "B. platyphylla", results$species)
+results$species <- sub("bpendula", "B. pendula", results$species)
+results$species <- sub("hann", "H. annus", results$species)
+results$species <- sub("hargo", "H. argophyllus", results$species)
+results$species <- sub("hpet", "H. petiolaris", results$species)
+results$species <- sub("capsella", "C. rubella", results$species)
+results$species <- sub("ahalleri", "A. halleri", results$species)
+results$species <- sub("athaliana", "A. thaliana", results$species)
+results$species <- sub("ptremula", "P. tremula", results$species)
+results$species <- sub("bstricta", "B. stricta", results$species)
+results$species <- sub("phalli", "P. hallii", results$species)
+results$species <- sub("atuber", "A. tuberculatus", results$species)
+results$species <- sub("mtruncatula", "M. truncatula", results$species)
+
+
 library(dplyr)
 data <- pic %>% left_join(results, by = c("Orthogroup"))
 
 
-p2<- ggplot(data, aes(x=reorder(species, ortho_DS, FUN = median), y=ortho_DS, fill = species)) +
-     geom_boxplot() + theme_classic()  + theme(legend.position = "none") + ylab("Ortho_DS\n") + ggtitle("Distribution of the OGs DS corrected emp p-values across species for the PicMin OGs with FDR < 0.5 (280 OGs)") + xlab("Species") +
-     scale_fill_manual(values=pal)
+p2<- ggplot(data, aes(x=reorder(species, ortho_DS, FUN = median), y=ortho_DS)) +
+     geom_boxplot(fill = viridis(1,option = "mako", direction = -1)) + theme_classic()  + theme(legend.position = "none") + ylab("Ortho_DS\n") + ggtitle("Distribution of the OGs DS corrected emp p-values across species for the PicMin OGs with FDR < 0.5") + xlab("Species")
 
 
 
@@ -82,19 +101,11 @@ contribution <- as.numeric(reformatted_data$below) / (as.numeric(reformatted_dat
 final_data <- cbind(reformatted_data,contribution)
 
 
-# p3 <- ggplot(final_data, aes(x = reorder(species, contribution),y = 1, fill = contribution)) + scale_fill_viridis_c(direction = 1, option  = "cividis") + theme_classic() +
-#   geom_tile(height =0.5) +
-#   theme(axis.title.y=element_blank(),
-#         axis.line.y=element_blank(),
-#         axis.text.y=element_blank(),
-#         axis.ticks.y=element_blank()) +
-#         xlab("Species") + labs(fill='Species contribution') + ggtitle("Species contribution to PicMin OGs with FDR < 0.4 --> N(OG-p < 0.1)/N(OG-p)")
 
-
-p4 <- ggplot(data=final_data, aes(x=reorder(species,contribution), y=contribution*100)) +
-geom_bar(stat="identity", width = 0.6,fill = "lightgoldenrod", col = "black") + coord_flip() + theme_classic() + xlab("Species") + ylab("Contribution %") + labs(fill='Species') + theme(legend.position = "none") +
+p3 <- ggplot(data=final_data, aes(x=reorder(species,contribution), y=contribution*100, fill = "constant") ) +
+geom_bar(stat="identity", width = 0.6, col = "black") + coord_flip() + theme_classic() +  scale_fill_viridis_d(direction = -1, option  = "mako") + xlab("Species") + ylab("Contribution %") + labs(fill='Species') + theme(legend.position = "none") +
 ggtitle("Species contribution to PicMin OGs with FDR < 0.5 --> N(OG-p < 0.1)/N(OG-p)")
 
 
 library(gridExtra)
-grid.arrange(p1, p4,p2,heights = c(1,1, 1))
+grid.arrange(p1, p3,p2,heights = c(1,1, 1))

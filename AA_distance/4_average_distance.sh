231bc7ce92e49a1a9a6ada1d62ac38e6e61d960f
Gabriele <g.nocchi@hotmail.com>
2023-04-17 10:40:37 -0600

    Add files via upload

--- AA_distance/4_average_distance.sh
+++ AA_distance/4_average_distance.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 for file in *.txt; do
   sed -i '1,6d' "$file"
-  sed -i 's/-nan/0/g' "$file"
-  result=$(awk 'NR>1{for(i=2;i<=NF-2;i++) if($i!=0) {sum += $i; count++}} END{print sum/count}' "$file")
+  result=$(awk 'NR>1{for(i=2;i<=NF-2;i++) if($i!="nan") {sum += $i; count++}} END{print sum/count}' "$file")
   echo "$file $result" >> output.txt
 done
 

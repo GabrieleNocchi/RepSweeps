e3ee7a2c9a03f710e131a3fabb7e83df80c44509
Gabriele <g.nocchi@hotmail.com>
2023-05-03 09:32:06 -0600

    Add files via upload

--- utilities/compare_5kb_1kb/plot_5k_1k.R
+++ utilities/compare_5kb_1kb/plot_5k_1k.R
@@ -1,4 +1,3 @@
-setwd("/users/gnocc/Desktop")
 library(dplyr)
 
 df <- read.table("to_plot_in_R.txt")
@@ -13,8 +12,18 @@ c <- cbind(b,a)
 
 colnames(c) <- c("window_ID1", "start", "end", "null","CLR_window", "SNP_count", "maximum_CLR_window", "window_ID2", "mean_CLR")
 
-plot(log10(b$V5), log10(b$V7), xlab = "log10 5kb CLR", ylab = "log10 1kb_max_CLR")
-cor(b$V5,b$V7)
 
 plot(log10(c$CLR_window), log10(c$mean_CLR), xlab = "log10 5kb CLR", ylab = "log10 1kb_average_CLR")
 cor(c$CLR_window,c$mean_CLR)
+
+
+
+library(dplyr)
+library(ggplot2)
+
+# c <- c %>%
+#   filter(CLR_window > 0 & mean_CLR > 0)
+
+
+p <- ggplot(c, aes(log10(CLR_window + 0.00001), log10(mean_CLR + 0.00001)))
+p + geom_point(shape =21, size = 3, color = "black", fill = "lightgoldenrod") + theme_classic() + xlab("log10 10kb windows (5kb spaced scans) CLR") + ylab("log10 1kb-calculated average CLR per 10kb window")

ab4784799b1902d06bb8d19a22a864a528fd8821
Gabriele <g.nocchi@hotmail.com>
2023-07-14 10:57:37 -0600

    Add files via upload

---
+++ pipeline/omegaplus_results_analysis/genes_only/3_sweed_results_analysis_genes_only_OmegaPlus.sh
@@ -0,0 +1,16 @@
+regex='(.+)	(.+)'
+
+
+while read p; do
+
+if [[ $p =~ $regex ]];  then tail -n +3 OmegaPlus_Report.kubota_ahalleri.vcf.gz_${BASH_REMATCH[2]} | awk -v var="${BASH_REMATCH[2]}" 'BEGIN {OFS="\t"} {print var, $2, 5}' > formatted_${BASH_REMATCH[2]}; fi
+
+done < genes_coord.txt
+
+
+
+cat formatted_* > final_genes_analysis.txt
+rm formatted*
+
+
+Rscript results_analysis.R

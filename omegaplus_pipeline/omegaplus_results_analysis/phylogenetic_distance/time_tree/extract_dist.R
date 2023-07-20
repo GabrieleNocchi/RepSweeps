f167ad969e3c587f9f22e74c03c8f243be164263
Gabriele <g.nocchi@hotmail.com>
2023-07-20 12:47:03 -0600

    Delete omegaplus_pipeline directory

--- omegaplus_pipeline/omegaplus_results_analysis/phylogenetic_distance/time_tree/extract_dist.R
+++
@@ -1,7 +0,0 @@
-library(ape)
-tree<-read.tree("alignment.nwk")
-my_m <-cophenetic(tree)
-
-my_m <-as.data.frame.table(my_m)
-
-write.table(my_m, file = "formatted_phylogenies.txt", row.names = FALSE, quote = FALSE)

9a04303d61f21b6c0188fce348a4be7ce939f670
Gabriele <g.nocchi@hotmail.com>
2023-11-06 15:49:11 -0700

    Add files via upload

---
+++ pipeline/omegaplus_pipeline/genes_only/2_genes_only_OmegaPlus_SLURM_1000.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=00-20:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=500M
+#SBATCH --account=def-yeaman
+#SBATCH --array=1-999
+
+
+
+GENEFILE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" genes_files_list.txt)
+
+module load bcftools
+
+
+
+
+
+#change file name accordingly
+FILE="kubota_ahalleri.vcf.gz"
+
+
+regex='(.+)	(.+)'
+
+while read p; do
+
+if [[ $p =~ $regex ]];  then 	bcftools view $FILE --regions ${BASH_REMATCH[1]} -Ov -o $FILE\_${BASH_REMATCH[2]}\.vcf; /home/gabnoc/projects/def-yeaman/gabnoc/OmegaPlus/OmegaPlus-M -input $FILE\_${BASH_REMATCH[2]}\.vcf -minwin 1000 -maxwin 100000 -grid 3 -name $FILE\_${BASH_REMATCH[2]} -seed 12345 -threads 2; fi
+	rm $FILE\_${BASH_REMATCH[2]}\.vcf
+done < $GENEFILE
+
+
---
+++ pipeline/omegaplus_results_analysis/genes_only/3_sweed_results_analysis_genes_only_OmegaPlus_500_1000.sh
@@ -0,0 +1,16 @@
+regex='(.+)	(.+)'
+
+
+while read p; do
+
+if [[ $p =~ $regex ]];  then tail -n +3 OmegaPlus_Report.kubota_ahalleri.vcf.gz_${BASH_REMATCH[2]} | awk -v var="${BASH_REMATCH[2]}" 'BEGIN {OFS="\t"} {print var, $2, 5}' > formatted_${BASH_REMATCH[2]}; fi
+
+done < genes_coord.txt
+
+
+
+cat formatted_* > final_genes_analysis.txt
+rm formatted*
+awk 'NR % 3 == 2' final_genes_analysis.txt  > final_genes_analysis_1.txt
+
+Rscript results_analysis.R

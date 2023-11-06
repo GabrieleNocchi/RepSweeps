ca2240c84d4c5b81115e4463cbfe6d5ab35c9f40
Gabriele <g.nocchi@hotmail.com>
2023-11-06 15:48:31 -0700

    Delete pipeline directory

--- pipeline/omegaplus_results_analysis/genes_only/r.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=0-24:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH --account=def-yeaman
-
-module load r/4.2.1
-module load bedtools
-./3_sweed_results_analysis_genes_only_OmegaPlus.sh
--- pipeline/sweed_pipeline/genes_only/create_genes_coord.sh
+++
@@ -1,6 +0,0 @@
-awk '{OFS="\t"}NR>1{print $1,$4-1000,$5+1000}' genes.gff | awk '{OFS="\t"}{if ($2 > 0)  print $1":"$2"-"$3; else print $1":"1"-"$3;}' > genes_coord.txt
-tail -n +2 genes.gff | awk '{print $1":"$4"-"$5}' > g
-paste genes_coord.txt g > merged
-rm g
-mv merged genes_coord.txt
-split --numeric-suffixes=1 -n l/999 -d genes_coord.txt splitted

536e093ee196113f971587d874eeff83c4ee24e5
Gabriele <g.nocchi@hotmail.com>
2023-11-06 15:40:28 -0700

    Update 4_average_distance.sh

--- aa_distance/4_average_distance.sh
+++ aa_distance/4_average_distance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for file in *.txt; do
   sed -i '1,6d' "$file"
-  result=$(awk 'NR>1{for(i=2;i<=NF-2;i++) if($i!="nan") {sum += $i; count++}} END{print sum/count}' "$file")
+  result=$(awk 'NR>1{for(i=2;i<=NF-2;i++) if($i!="nan" && $i!="-nan") {sum += $i; count++}} END{print sum/count}' "$file")
   echo "$file $result" >> output.txt
 done
 
